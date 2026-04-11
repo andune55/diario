84247e09042177b50eb9a790be0873f1c8184fe0
Agustin <latierramedia@gmail.com>
2026-04-11 15:07:19 +0200

    cambio base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"https://andune55.github.io/diario/",
+  base:"/diario/",
   plugins: [
     react(),
     tailwindcss()],
